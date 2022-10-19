e87402e209dde3bec24dcad4974cc92cbe634ae7
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-10-19 20:53:16 -0300

    Configurações básicas do projeto feitas

---
+++ src/Constants/Constants.js
@@ -0,0 +1,8 @@
+export const AZUL_CLARO = "#52B6FF";
+
+export const AZUL_ESCURO = "#126BA5";
+
+export const VERDE = "#8FC549";
+
+export const URL_BASE =
+  "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit";
