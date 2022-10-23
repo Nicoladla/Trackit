7e1bd5b2f84583b6f1c8e498591c326ba2eff296
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-10-23 18:43:22 -0300

    Layout da tela de histórico, feita.

--- src/Pages/Historico/Historico.js
+++ src/Pages/Historico/Historico.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export default function Historico(){
     return(
         <TelaHistorico>
-            Habitos
+            <h1>Histórico</h1>
+            <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
             </TelaHistorico>
     )
 }
@@ -11,5 +12,12 @@ export default function Historico(){
 const TelaHistorico = styled.div`
   background-color: #f2f2f2;
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
+  padding: 100px 18px;
+
+  p {
+    margin-top: 17px;
+    font-size: 18px;
+    line-height: 22px;
+  }
 `;
