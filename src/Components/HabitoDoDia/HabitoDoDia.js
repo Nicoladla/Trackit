1a28a32b79ef172ceb01c3d0a8ff773f03d33ac5
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-10-24 13:02:13 -0300

    Layout da tela hoje, feita.

---
+++ src/Components/HabitoDoDia/HabitoDoDia.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export default function HabitoDoDia() {
+  return (
+    <Habito>
+      <div>
+        <h3>Ler 1 capítulo de livro</h3>
+        <p>Sequência atual: 4 dias</p>
+        <p>Seu recorde: 5 dias</p>
+      </div>
+      <div>
+        <ion-icon name="checkbox"></ion-icon>
+      </div>
+    </Habito>
+  );
+}
+
+const Habito = styled.article`
+  background-color: white;
+  width: 100%;
+  min-height: 94px;
+  padding: 13px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h3 {
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 13px;
+    line-height: 16px;
+  }
+
+  ion-icon {
+    color: #EBEBEB;;
+    font-size: 69px;
+  }
+`;
--- src/Pages/Hoje/Hoje.js
+++ src/Pages/Hoje/Hoje.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
+import HabitoDoDia from "../../Components/HabitoDoDia/HabitoDoDia";
 
 export default function Hoje() {
   return (
     <TelaHoje>
-      <div>Hoje</div>
+      <h1>Segunda, 17/05</h1>
+      <h2>Nenhum hábito concluído ainda</h2>
+
+      <HabitoDoDia/>
     </TelaHoje>
   );
 }
@@ -11,5 +15,12 @@ export default function Hoje() {
 const TelaHoje = styled.div`
   background-color: #f2f2f2;
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
+  padding: 100px 18px;
+
+  h2{
+    margin: 8px 0 28px 0;
+    font-size: 18px;
+    color: #BABABA;
+  }
 `;
